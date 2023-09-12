4f67c7a0a2760462272f35161e3dbcdbe2a9ccd1
Maycaela Enguito <maycaelaenguito@gmail.com>
2023-09-12 07:25:33 -0400

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ import About from './components/About';
 import Blog from "./components/Blog";
 import Services from "./components/Services";
 import Contactus from "./components/Contactus";
+import Shop from './components/Shop';
+import Booking from './components/Booking';
+import Gallery from './components/Gallery';
+
 
 function App() {
 
@@ -13,10 +17,14 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route exact="true" path="/" element={<Home/>}></Route>
-          <Route exact="true" path="/about" element={<About/>}></Route>
-          <Route exact="true" path="/services" element={<Services/>}></Route>
-          <Route exact="true" path="/contact" element={<Contactus/>}></Route>
-          <Route exact="true" path="/blog" element={<Blog/>}></Route>
+          <Route path="/about" element={<About/>}></Route>
+          <Route path="/services" element={<Services/>}></Route>
+          <Route path="/contact" element={<Contactus/>}></Route>
+          <Route path="/blog" element={<Blog/>}></Route>
+          <Route path="/shop" element={<Shop/>}></Route>
+          <Route path="/booking" element={<Booking/>}></Route>
+          <Route path="/gallery" element={<Gallery/>}></Route>
+
         </Routes>
      </BrowserRouter>
     </div>
--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,12 +1,12 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
 
-const Services = () => {
+const Gallery = () => {
   return (
     <div>
-      <h1>Services</h1>
+      <h1>Gallery</h1>
     </div>
   )
 }
 
-export default Services
+export default Gallery
