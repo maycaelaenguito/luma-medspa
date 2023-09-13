c65ab60022f4d91b4571863fe9fedec5632a7699
Maycaela Enguito <maycaelaenguito@gmail.com>
2023-09-13 04:01:29 -0400

    commit

--- src/components/BookBtn.jsx
+++ src/components/BookBtn.jsx
@@ -7,7 +7,7 @@ const BookBtn = () => {
     <div>
       <div>
         <Link to='/booking'>
-            <button className='uppercase tracking-widest border-2 border-[#EFB2B1] px-4 py-2 rounded-lg hover:bg-[#EFB2B1] transition duration-300'>
+            <button className='uppercase tracking-widest border-2 border-[#EFB2B1] px-4 py-2 rounded-sm hover:bg-[#EFB2B1] transition duration-300'>
                 Book Now
             </button>
         </Link>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,15 +13,15 @@ const Home = () => {
               <Navbar/>
               <MobileNav/>
         </div>
-        <div className="flex h-[90vh] lg:h-auto items-center lg:items-end justify-center header-content lg:pt-[3rem]">
+        <div className="flex h-[90vh] lg:h-auto items-center lg:items-end justify-center header-content lg:pt-[7rem] xl:pt-[3rem]">
             <div>
                 <h1 className='text-slate-100 font-[300] tracking-wider lg:text-[120px] text-center lg:leading-[10rem]'>Welcome to <br/> <span className='text-black'>LUMA</span> <span className='text-[#F2A8A6]'>Medspa</span></h1>
                 <div className='flex justify-center'>
-                  <p className=' text-center lg:text-[80px] text-slate-100 leading-none'>Take care of your beauty and <br/> wellness</p>
+                  <p className=' text-center lg:text-[50px] xl:text-[60px] text-slate-100 leading-none'>Take care of your beauty and <br/> wellness</p>
                 </div>
                 <div className='flex justify-center w-full lg:pt-[3rem]'>
                   <Link>
-                      <button className='uppercase tracking-wider border-2 border-[#EFB2B1] px-6 py-2 rounded-lg hover:bg-[#EFB2B1] transition duration-300 text-slate-100 font-regular lg:text-[25px]'>
+                      <button className='uppercase tracking-wider border-2 border-[#EFB2B1] px-6 py-2 rounded-sm hover:bg-[#EFB2B1] transition duration-300 text-slate-100 font-regular lg:text-[25px]'>
                           Book Now
                       </button>
                   </Link>
