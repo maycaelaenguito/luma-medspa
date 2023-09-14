c439e518c34e9c470b8dea005b4d474628b23d02
Maycaela Enguito <maycaelaenguito@gmail.com>
2023-09-14 05:57:07 -0400

    commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,15 +13,15 @@ const Home = () => {
               <Navbar/>
               <MobileNav/>
         </div>
-        <div className="flex h-[90vh] lg:h-auto items-center lg:items-end justify-center header-content lg:pt-[7rem] xl:pt-[3rem]">
+        <div className="flex h-[90vh] lg:h-[65vh] items-center lg:items-end justify-center header-content lg:pt-[7rem] xl:pt-[3rem]">
             <div>
-                <h1 className='font-raleway text-slate-100 font-[300] tracking-wider text-[12vw] sm:text-[80px] md:text-[100px] lg:text-[120px] text-center lg:leading-[10rem]'>Welcome to <br/> <span className='text-black'>LUMA</span> <span className='text-[#F2A8A6]'>Medspa</span></h1>
+                <h1 className='font-raleway text-slate-100 font-[300] tracking-wider text-[12vw] sm:text-[80px] md:text-[100px] xl:text-[120px] text-center lg:leading-[17vh]'>Welcome to <br/> <span className='text-black'>LUMA</span> <span className='text-[#F2A8A6]'>Medspa</span></h1>
                 <div className='flex justify-center'>
-                  <p className='font-Dancing text-center text-[6.5vw] sm:text-[35px] lg:text-[50px] xl:text-[60px] text-slate-100 leading-none'>Take care of your beauty and <br/> wellness</p>
+                  <p className='font-Dancing text-center text-[6.5vw] sm:text-[35px] md:text-[40px] xl:text-[60px] text-slate-100 leading-none'>Take care of your beauty and <br/> wellness</p>
                 </div>
                 <div className='flex justify-center w-full lg:pt-[3rem]'>
                   <Link>
-                      <button className='uppercase tracking-wider border-2 border-[#EFB2B1] px-6 py-2 rounded-sm hover:bg-[#EFB2B1] transition duration-300 text-slate-100 font-regular lg:text-[25px]'>
+                      <button className='uppercase tracking-wider border-2 border-[#EFB2B1] px-6 py-2 rounded-sm hover:bg-[#EFB2B1] transition duration-300 text-slate-100 font-regular lg:text-[20px]'>
                           Book Now
                       </button>
                   </Link>
