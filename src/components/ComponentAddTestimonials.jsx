5de238b07ba99b87e7c18e698edb694d1ff70154
Maycaela Enguito <maycaelaenguito@gmail.com>
2023-09-18 01:38:00 -0400

    commit

--- src/components/ComponentAddTestimonials.jsx
+++ src/components/ComponentAddTestimonials.jsx
@@ -2,6 +2,7 @@
 import React, { useState } from 'react';
 // eslint-disable-next-line no-unused-vars
 import axios from 'axios'; // If you are using a backend for storing data.
+import { Link } from 'react-router-dom';
 
 function ComponentAddTestimonials() {
   const [clientData, setClientData] = useState({
@@ -83,7 +84,8 @@ function ComponentAddTestimonials() {
     <div className="container mx-auto p-4">
       <div className='grid lg:grid-cols-3 gap-5'>
         {/* Form */}
-        <form className='lg:rid-cols-1' onSubmit={handleSubmit}>
+       <div className='lg:col-span-1'>
+          <form  onSubmit={handleSubmit}>
             <div className="mb-4">
               <label className="block text-gray-700 font-medium mb-2" htmlFor="name">
                 Client Name
@@ -128,34 +130,61 @@ function ComponentAddTestimonials() {
               {isEditing ? 'Update' : 'Post'}
             </button>
           </form>
+       </div>
           {/* Testimonials */}
-          <div className="mt-8 lg:grid-cols-2">
+          <div className="mt-8 lg:col-span-2">
             {clients.map((client, index) => (
-              <div key={index} className="mb-4">
-                <div className="bg-gray-100 p-4 rounded">
-                  <p className="font-semibold">{client.name}</p>
-                  <p>{client.comment}</p>
-                  {client.image && (
-                    <img
-                      src={URL.createObjectURL(client.image)}
-                      alt={client.name}
-                      className="mt-2 max-w-full"
-                    />
-                  )}
+              <div key={index} className="mb-4 flex flex-row gap-2 ">
+                <div className="flex flex-row-reverse gap-2 bg-gray-300 p-4 rounded-md">
+                  <div className=' w-[35vw] pt-3'>
+                    <div className='relative cursor-pointer group'>
+                      <p className="font-semibold">{client.name}</p>
+                      <div>
+
+                      </div>
+                    </div>
+                    <div className='cursor-pointer'>
+                      <p>{client.comment}</p>
+                    </div>
+                  </div>
+                  <div >
+                      <Link>
+                        <div className='cursor-pointer relative group'>
+                          {client.image && (
+                            <img
+                              src={URL.createObjectURL(client.image)}
+                              alt={client.name}
+                              className="mt-2 w-[100px] rounded-full group-hover:brightness-75"
+                            />
+                          )}
+                           <div className='absolute top-9 left-8 text-white'>
+                            <Link className='hidden group-hover:flex hover:underline'>Edit</Link>
+                           </div>
+                        </div>
+                      </Link>
+                  </div>
                 </div>
-                <div className="mt-2">
-                  <button
-                    onClick={() => handleEdit(index)}
-                    className="text-blue-500 hover:underline mr-2"
-                  >
-                    Edit
-                  </button>
-                  <button
-                    onClick={() => handleDelete(index)}
-                    className="text-red-500 hover:underline"
-                  >
-                    Delete
-                  </button>
+                <div className="mt-2 flex items-center text-sm">
+                  <div className='flex flex-row gap-3'>
+                    <button
+                      onClick={() => handleEdit(index)}
+                      className=" hover:underline"
+                    >
+                      Edit
+                    </button>
+                    <button
+                      onClick={() => handleDelete(index)}
+                      className="hover:underline"
+                    >
+                      Delete
+                    </button>
+                    <button
+                      onClick={() => handleDelete(index)}
+                      className=" hover:underline"
+                    >
+                      Publish
+                    </button>
+                  </div>
                 </div>
               </div>
             ))}
