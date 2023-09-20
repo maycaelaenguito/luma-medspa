9330aa2d876053c111b5db1bcf2796b7cebc0598
Maycaela Enguito <maycaelaenguito@gmail.com>
2023-09-20 01:12:12 -0400

    commit

--- src/components/MobileNav.jsx
+++ src/components/MobileNav.jsx
@@ -1,33 +1,47 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Squash as Hamburger } from 'hamburger-react';
-import TopNav from './TopNav';
-
 
 const MobileNav = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div className='xl:hidden relative'>
-      <div className=' w-full h-[65px] bg-stone-950 flex flex-row justify-between items-center px-[1rem]'>
+      <div className='w-full h-[65px] bg-stone-950 flex flex-row justify-between items-center px-[1rem]'>
         <div className="logo-container w-[full] flex justify-left">
-            <Link to="/">
-                <div className='logo-container w-[55px]'>
-                    <img src="https://res.cloudinary.com/dk3aalcb0/image/upload/v1694409716/Luma%20Medspa/Luma_Logo_White_1_o5wuky.png" alt="" />
-                </div>
-            </Link>
+          <Link to="/">
+            <div className='logo-container w-[55px]'>
+              <img src="https://res.cloudinary.com/dk3aalcb0/image/upload/v1694409716/Luma%20Medspa/Luma_Logo_White_1_o5wuky.png" alt="" />
+            </div>
+          </Link>
         </div>
-        <div> 
-            <Hamburger 
+        <div>
+          <Hamburger
             size={24}
             color='white'
-            />
+            toggled={isOpen}
+            toggle={toggleMenu}
+          />
         </div>
       </div>
-      <div className='absolute '>
-        <TopNav/>
+      <div className={`absolute w-full left-full ${isOpen ? 'left-[0]' : 'left-full'}`}>
+        <ul className='bg-slate-100 py-5 w-full flex flex-col gap-3'>
+          <li><Link>Home</Link></li>
+          <li><Link>About</Link></li>
+          <li><Link>Services</Link></li>
+          <li><Link>Contact</Link></li>
+          <li><Link>Blog</Link></li>
+          <li><Link>Shop</Link></li>
+          <li><Link>Gallery</Link></li>
+        </ul>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default MobileNav
+export default MobileNav;
