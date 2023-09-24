315702cbbbfc29311645fcfc6fed8be9b6da1093
Maycaela Enguito <maycaelaenguito@gmail.com>
2023-09-23 21:59:11 -0400

    comit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,22 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='w-full bg-stone-950 h-auto text-white'>
-      <h1>HEllo</h1>
-    </div>
+    <footer className='w-full bg-stone-950 h-auto text-white py-10 xl:px-[16.6%] px-[10%]'>
+      <div className='grid lg:grid-cols-4'>
+        <div>
+          Logo Here
+        </div>
+        <div>
+          <h1>Quick Links</h1>
+        </div>
+        <div>
+          <h1>Services</h1>
+        </div>
+        <div>
+          <h1>Contact</h1>
+        </div>
+      </div>
+    </footer>
   )
 }
 
