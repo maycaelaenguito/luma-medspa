2ce4c30974135c91ebd6752535b2d7c8c98f8f07
Maycaela Enguito <maycaelaenguito@gmail.com>
2023-10-15 21:27:30 -0400

    commit

--- src/components/OfferingsScroll.jsx
+++ src/components/OfferingsScroll.jsx
@@ -12,7 +12,7 @@ const OfferingsScroll = () => {
 
   const variants = {
     hidden: { opacity: 0, y: 20 },
-    visible: { opacity: 1, y: 0, transition: { duration: 1.5, delay: 0.1 } },
+    visible: { opacity: 1, y: 0, transition: { duration: 1.5, delay: 0 } },
   };
 
   return (
