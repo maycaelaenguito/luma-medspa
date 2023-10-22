b82c1d68a43132b6faef5e855d40ebc68ccf1d12
Maycaela Enguito <maycaelaenguito@gmail.com>
2023-10-22 17:21:58 -0400

    commit

---
+++ src/components/AddToCartBtn.jsx
@@ -0,0 +1,16 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import {BsCartFill} from 'react-icons/bs'
+
+const AddToCartBtn = () => {
+  return (
+    <div className='flex w-full justify-center'>
+        <button className='bg-[#EFB2B1] text-white text-sm lg:text-lg md:text-base xl:text-lg px-6 py-3 hover:bg-black transition-all duration-300 rounded-lg my-4 tracking-wide font-raleway font-semibold flex flex-row justify-center items-center gap-2'>
+            <BsCartFill size={24}/> 
+            Add to cart
+        </button>
+    </div>
+  )
+}
+
+export default AddToCartBtn
--- src/components/HomeProducts.jsx
+++ src/components/HomeProducts.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
 import { useInView } from 'react-intersection-observer';
-import AddToCart from './AddToCart';
+import AddToCart from './AddToCartBtn';
 
 const HomeProducts = () => {
   // Create an array of refs
